49e264fb97cfeb6a2077c8f5207adea048d5f1c6
Matthew Hargraves <Matthew@greyhoundit.co.uk>
2016-01-23 18:51:28 +0000

    added posts and some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'static/home'
   get 'static/finals'
   get 'static/rules'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230174923) do
+ActiveRecord::Schema.define(version: 20160123171014) do
 
   create_table "clubs", force: true do |t|
     t.string   "name"
     t.string   "zone"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "news", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "submissions", force: true do |t|
