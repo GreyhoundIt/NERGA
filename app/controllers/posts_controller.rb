54734bbe3a3eb14a46e3d65937880e89fca34c65
Matthew Hargraves <Matthew@greyhoundit.co.uk>
2016-02-12 09:27:27 +0000

    migrationfile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-before_filer :authenticate_user!, except: [ :index, :show ]
+before_action :authenticate_user!, except: [ :index, :show ]
   respond_to :html
 
   def index
