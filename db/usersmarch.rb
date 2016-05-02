f53c9e28308dcb1d1ef2c452534933b07c201fcf
Matthew Hargraves <Matthew@greyhoundit.co.uk>
2016-05-02 16:02:39 +0100

    results

--- db/usersmarch.rb
+++ db/usersmarch.rb
@@ -2,3 +2,5 @@ User.create(:email => "Colin@shireaggregates.co.uk", :password => "NERGA2016", :
 User.create(:email => "johncoleman_2000@yahoo.com", :password => "NERGA2016", :password_confirmation => "NERGA2016", :club => 'Darlington ')
 User.create(:email => "geoffhall160373@outlook.com", :password => "NERGA2016", :password_confirmation => "NERGA2016", :club => 'Darlington ')
 User.create(:email => "everton.mann9@gmail.com", :password => "NERGA2016", :password_confirmation => "NERGA2016", :club => 'Darlington ')
+User.create(:email => "anthonyjhall63@hotmail.com", :password => "NERGA2016", :password_confirmation => "NERGA2016", :club => 'Wearside')
+User.find_by_email("abc@xyz.com")
